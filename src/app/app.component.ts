55d856b389b5aa11b7fcf69598c10d5af5a10543
arturzrk <Artur@Developers-MacBook-Pro.local>
2018-07-26 20:06:24 +0200

    delete action added to the list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, AngularFireAction } from 'angularfire2/database';
+import { Subscription, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { DatabaseReference, DatabaseSnapshot } from '../../node_modules/angularfire2/database/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +10,37 @@ import { AngularFireDatabase } from 'angularfire2/database';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items: any[];
-  constructor(db: AngularFireDatabase) {
-    db.list('/courses').valueChanges()
-      .subscribe(items => {
-        this.items = items;
-        console.log(this.items);
+  courses$: Observable<{}[]>;
+  courseswithKeys$: Observable<any[]>;
+  courseList: AngularFireList<any>;
+  course$;
+  author$;
+  coursesSnapshot: Observable<AngularFireAction<DatabaseSnapshot<any>>[]>;
+  title = 'Firebase demo';
+
+  constructor(private db: AngularFireDatabase) {
+    this.courseList = this.db.list('/courses');
+    this.courses$ = db.list('/courses').valueChanges();
+    this.coursesSnapshot = this.courseList.snapshotChanges();
+    this.course$ = db.object('/courses/1').valueChanges();
+    this.author$ = db.object('/authors/1').valueChanges();
+    this.courseswithKeys$ =  this.coursesSnapshot.pipe(
+      map(courses =>
+        courses.map(course => ({key: course.payload.key, value: course.payload.val()}))
+    ));
+    this.courseswithKeys$.subscribe( courses => {
+      console.log('courses:');
+      console.log(courses);
     });
+  }
+
+  add( course: HTMLInputElement) {
+    this.courseList.push(course.value);
+    console.log('adding course ...', course.value);
+  }
+
+  delete(key: string) {
+    console.log('deleting ...', key);
+    this.courseList.remove(key);
   }
 }
